786000907f7f3df61007d41caa2ca65c5802cf7d
Anzhelika Gakal <gakal.a@mail.ru>
2025-05-08 16:06:35 +0000

    edits

--- TradeWiseBackend.Dal/Repositories/StrategyRepository.cs
+++ TradeWiseBackend.Dal/Repositories/StrategyRepository.cs
@@ -11,23 +11,13 @@ public class StrategyRepository(DatabaseContext dbContext) : IStrategyRepository
 {
     public async Task SaveStrategyStages(List<StrategyStage> strategyStages)
     {
-        Console.WriteLine("KEKE1 ");
-        foreach (var stage in strategyStages)
-        {
-            Console.WriteLine(stage.User.Id);
-        }
         var strategyStageEntities = strategyStages.Adapt<List<StrategyStageEntity>>();
         foreach (var stageEntity in strategyStageEntities)
         {
             var originalStage = strategyStages.First(s => s.StageId == stageEntity.StageId);
 
             stageEntity.UserId = originalStage.User.Id;
-        }
-        Console.WriteLine("KEKE2 ");
-         foreach (var stage in strategyStageEntities)
-        {
-            Console.WriteLine(stage.UserId);
-            Console.WriteLine(stage.User.Id);
+            stageEntity.User = null;
         }
 
         await dbContext.StrategyStages.AddRangeAsync(strategyStageEntities);
