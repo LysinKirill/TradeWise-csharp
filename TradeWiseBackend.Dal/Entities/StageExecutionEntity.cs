667be76862eb0595162c92e0adb2c82810cceb34
Anzhelika Gakal <gakal.a@mail.ru>
2025-05-20 18:08:05 +0000

    edits

--- TradeWiseBackend.Dal/Entities/StageExecutionEntity.cs
+++ TradeWiseBackend.Dal/Entities/StageExecutionEntity.cs
@@ -8,18 +8,18 @@ public class StageExecutionEntity
 {
     [Key]
     public Guid Id { get; set; }
-    
+
     [ForeignKey(nameof(Stage))]
     public Guid StageId { get; set; }
     public required StrategyStageEntity Stage { get; set; }
-    
+
     [ForeignKey(nameof(StrategyExecution))]
     public Guid ExecutionId { get; set; }
     public required StrategyExecutionEntity StrategyExecution { get; set; }
-    
+
     public required string Status { get; set; }
-    
+
     public DateTime CreatedAt { get; set; }
-    
+
     public DateTime UpdatedAt { get; set; }
 }
---
+++ TradeWiseBackend.Domain/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TradeWiseBackend.Domain.Interfaces.Repositories;
+
+public interface IUnitOfWork : IDisposable
+{
+    Task BeginTransactionAsync();
+    Task CommitAsync();
+    Task RollbackAsync();
+}
