634e889de1b81bb359d3dce68b9d21e9c0bc5a9f
Anzhelika Gakal <gakal.a@mail.ru>
2025-05-21 05:57:26 +0000

    add migration

---
+++ TradeWiseBackend.Dal/Migrations/20250521055601_RenameColumnsAndChangeTypes.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TradeWiseBackend.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumnsAndChangeTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModelName",
+                table: "StrategyStages");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "StageSourceId",
+                table: "StrategyTransitions",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "StageDestinationId",
+                table: "StrategyTransitions",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "StageModel",
+                table: "StrategyStages",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "StrategyExecutions",
+                type: "integer",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "StageExecutions",
+                type: "integer",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StageModel",
+                table: "StrategyStages");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "StageSourceId",
+                table: "StrategyTransitions",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "StageDestinationId",
+                table: "StrategyTransitions",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModelName",
+                table: "StrategyStages",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "StrategyExecutions",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "StageExecutions",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldMaxLength: 255);
+        }
+    }
+}
