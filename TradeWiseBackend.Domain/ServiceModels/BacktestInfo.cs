9fea9580b2d23fc60c5b64d6c38740309e650138
Anzhelika Gakal <gakal.a@mail.ru>
2025-05-27 09:12:43 +0000

    edit all-backtests

--- TradeWiseBackend.Domain/ServiceModels/BacktestInfo.cs
+++ TradeWiseBackend.Domain/ServiceModels/BacktestInfo.cs
@@ -1,15 +1,15 @@
 namespace TradeWiseBackend.Domain.ServiceModels;
 
 public record class BacktestInfo(
-    long BacktestId,
-    DateTime StartedAt,
-    DateTime FinishedAt,
-    DateTime TestPeriodStart,
-    DateTime TestPeriodEnd,
-    BacktestStatus Status,
-    double Profit,
-    int TradesCount,
-    double InitialBalance,
-    double FinalBalance,
-    DateTime CreatedAt
+    long? BacktestId,
+    DateTime? StartedAt,
+    DateTime? FinishedAt,
+    DateTime? TestPeriodStart,
+    DateTime? TestPeriodEnd,
+    BacktestStatus? Status,
+    double? Profit,
+    int? TradesCount,
+    double? InitialBalance,
+    double? FinalBalance,
+    DateTime? CreatedAt
 );
